f81e237f46b63ffdc15111208e0219bf682cda06
jalenjaloney <jalendeloney@gmail.com>
2026-04-15 23:46:49 -0700

    fixed ports

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -10,9 +10,9 @@ time ls data/* | parallel sh load_denormalized.sh {}
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time ls data/* | parallel python3 load_tweets.py --db=postgresql://postgres:pass@localhost:20002/postgres --inputs={}
+time ls data/* | parallel python3 load_tweets.py --db=postgresql://postgres:pass@localhost:5079/postgres --inputs={}
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time ls data/* | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:20003/postgres --inputs={}
+time ls data/* | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:6079/postgres --inputs={}
