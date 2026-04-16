15d2a833556c59a04f827af29ebec25185ef612a
jalenjaloney <jalendeloney@gmail.com>
2026-04-16 00:18:13 -0700

    fixed time output

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -5,14 +5,15 @@ files=$(find data/*)
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-time ls data/* | parallel sh load_denormalized.sh {}
+time sh -c 'ls data/* | parallel sh load_denormalized.sh {}'
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-time ls data/* | parallel python3 load_tweets.py --db=postgresql://postgres:pass@localhost:5079/postgres --inputs={}
+time sh -c 'ls data/* | parallel python3 load_tweets.py --db=postgresql://postgres:pass@localhost:5079/postgres --inputs={}'
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-time ls data/* | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:6079/postgres --inputs={}
+time sh -c 'ls data/* | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:6079/postgres --inputs={}'
+
